b8f62790cee759723bfe219db368c35ffb4c03f2
Sarailly <sarailly1987@gmail.com>
2025-05-18 19:36:42 +0330

    prime-number

---
+++ prime-number/script.js
@@ -0,0 +1,18 @@
+
+let number = +prompt("enter the number","0");
+
+if(number % 2 === 0){
+    alert("even");
+}
+if(number % 2 === 1){
+    alert("odd");
+}
+
+
+
+// let number1 = +prompt("enter the number1","0");
+// let number2 = +prompt("enter the number2","0");
+// let number3 = +prompt("enter the number3","0");
+
+// let average = (number1 +number2 +number3)/3 ;
+// alert(average);
