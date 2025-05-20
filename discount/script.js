5473590c0b2d0a37fcbb47d991ba1040cc8fa3cc
Sarailly <sarailly1987@gmail.com>
2025-05-20 14:33:58 +0330

    discount

---
+++ discount/script.js
@@ -0,0 +1,10 @@
+
+let price = +prompt("لطفا مبلغ را وارد نمایید","0")
+let discount = +prompt("لطفا تخفبف  را وارد نمایید","0")
+
+let finalPrice = price - (price/100) * discount;
+ alert(finalPrice)
+
+
+
+
